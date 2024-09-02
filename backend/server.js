7923282f1668bdf279b87b0bdf5482fd4e553695
asjad449 <134190836+asjad449@users.noreply.github.com>
2024-09-02 17:04:01 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -18,7 +18,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: ["https://quizzie-six-three.vercel.app","http://localhost:3000"],
+    origin: ["https://quizzie-six.vercel.app","http://localhost:3000"],
     credentials: true,
   })
 );
